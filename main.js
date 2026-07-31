4dc6883f677600d82d0133251648e89c134f36b9
solstition <svlrcert@gmail.com>
2026-07-31 13:35:25 +1000

    Removed navhover code
    Moving UI functions from main.js into ui-helper for easier, bug fixing, readability and code cleanliness.

--- main.js
+++ main.js
@@ -325,18 +325,6 @@ function initChecklist() {
 }
 
 // ============================================
-// NAV HOVER
-// ============================================
-document.querySelectorAll("ul li a").forEach((link) => {
-  const original = link.textContent;
-  const hover = link.dataset.hover;
-  if (hover) {
-    link.addEventListener("mouseenter", () => (link.textContent = hover));
-    link.addEventListener("mouseleave", () => (link.textContent = original));
-  }
-});
-
-// ============================================
 // MAIN INITIALIZATION
 // ============================================
 function init() {
